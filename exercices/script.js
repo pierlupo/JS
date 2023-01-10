8e3440535f203ce1eb0eff349d245e041262be3b
Kraimps Pierre <pier_lupo@laposte.net>
2023-01-10 12:32:59 +0100

    commitexo3

--- exercices/script.js
+++ exercices/script.js
@@ -1,25 +1,57 @@
-//la concaténation:
+// //la concaténation:
 
-let toto = "Pierre";
-let verbe = "débute";
-let lang = "programmation";
+// let toto = "Pierre";
+// let verbe = "débute";
+// let lang = "programmation";
 
-//let maPremiereSaisieTextuelle = toto + " " + verbe +  " la " + lang + " en JavaScript !"; // Pre ES6
-let maPremiereSaisieTextuelle = `${toto} ${verbe} la ${lang} en JavaScript !`; // Post ES6
+// //let maPremiereSaisieTextuelle = toto + " " + verbe +  " la " + lang + " en JavaScript !"; // Pre ES6
+// let maPremiereSaisieTextuelle = `${toto} ${verbe} la ${lang} en JavaScript !`; // Post ES6
 
-console.log(maPremiereSaisieTextuelle);
+// console.log(maPremiereSaisieTextuelle);
 
-//Exercice2 :
+// //Exercice2 :
 
-let x = 5;
-let y = 10;
+// let x = 5;
+// let y = 10;
 
-let sum = x + y;
+// let sum = x + y;
 
-console.log(sum);
-let maSomme = `la somme de ${x} + ${y} est égale à  ${sum}`; // Post ES6
-console.log(maSomme);
+// console.log(sum);
+// let maSomme = `la somme de ${x} + ${y} est égale à  ${sum}`; // Post ES6
+// console.log(maSomme);
 
-alert( `la somme de ${x} + ${y} est égale à ${sum}`);
+// alert( `la somme de ${x} + ${y} est égale à ${sum}`);
 
-prompt(`la somme de ${x} + ${y} est égale à : `);
+// let   nb1 = 9,
+//       nb2 =1,
+//       resultat,
+//       affichage;
+
+// nb1 = Number(prompt(`Veuillez saisir un nombre : `));
+// nb2 = Number(prompt(`Veuillez saisir un nombre : `));
+
+// resultat = nb1+nb2;
+
+// affichage = `la somme de ${nb1} + ${nb2} est égale à : ${resultat}`;
+
+// console.log(affichage)
+
+// alert(affichage);
+
+//exercice3:
+
+let aire,
+    diametre, 
+    affichage,
+    perimetre;
+
+const pi = Math.PI
+
+diametre =  Number(prompt(`Veuillez saisir un diametre : `));
+perimetre = diametre * pi;
+aire = pi * Math.pow((diametre/2),2);
+
+affichage = `le périmètre du cercle est égale à : ${perimetre} et l'aire est égale à : ${aire}`;
+
+console.log(affichage);
+alert(affichage);
